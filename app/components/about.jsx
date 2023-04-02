bf90deb0bbfa0092808fd7b053adf321307188c6
Exe-quiel <exe.quiel.developer@outlook.com>
2023-04-02 20:12:16 -0300

    Update About Me

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -10,8 +10,7 @@ function About() {
         <div className='sm:flex sm:flex-col md:grid md:grid-cols-2 items-center justify-center p-3 bg-gray-300 rounded-xl'>
           <img src={imagen} alt='foto desarrollador' className='w-full border-2 shadow-lg' />
           <div className='p-8'>
-            <p className='w-full text-xl mx-3 md:my-5 sm:border-y md:border-none p-2'>Hola! soy Mariano, soy <span className='font-bold text-violet-500'>desarrollador web</span> FrontEnd vivo en Buenos Aires, Argentina. Me gusta ampliar constantemente mis conocimientos para poder ampliar mi area laboral, actualmente sigo estudiando para poder convertirme en desarrollador <span className='font-bold text-violet-500'>Fullstack</span>.</p>
-            <p className='w-full text-xl mx-3 md:my-5 sm:border-y md:border-none p-2'><span className='font-bold text-violet-500 block text-center'> Hobbies </span>Me gusta dibujar, escuchar musica y jugar videojuegos en mis tiempos libres. Mi fuerte de dibujo son las caligrafias y el graffiti.</p>
+            <p className='w-full text-xl mx-3 md:my-5 sm:border-y md:border-none p-2'>Hola! Me llamo Mariano, soy <span className='font-bold text-violet-500'>desarrollador web</span> FrontEnd, vivo en Buenos Aires, Argentina. Busco una oportunidad para aplicar mis conocimientos aprendidos en mi formacion academica con el fin de beneficiar a una empresa y desarrollarme de forma profesional y personal. Tengo como objetivo volverme un experto en el area.</p>
           </div>
         </div>
       </div>
